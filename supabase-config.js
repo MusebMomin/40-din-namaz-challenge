b4cbc363d128fdea80f947210bc99c5a8fdd25c6
Museb Momin <42113545+MusebMomin@users.noreply.github.com>
2026-06-14 17:12:44 +0530

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,1 +1,4 @@
-Hi
+window.SUPABASE_CONFIG = {
+    url: 'https://aoqnqcqhkeqzfbackzpk.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFvcW5xY3Foa2VxemZiYWNrenBrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE0MjE4MzgsImV4cCI6MjA5Njk5NzgzOH0.aRxSz4NoRUgoY5bi25k2pyc34PPLkaDCJGip7R4K7go'
+};
